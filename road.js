b1704982402c70aba106af73ab504a5dd89ceb98
Ahmed Farrag <47854268+Ahmed-Farrag@users.noreply.github.com>
2023-03-05 21:38:11 +0200

    setup

---
+++ road.js
@@ -0,0 +1,20 @@
+// make structure :
+//    -client folder : React
+//    -server folder : node
+
+// in react :
+/**
+ * create application
+ * create component
+ * pages component 
+ * to redux make data, store folder
+ */ 
+
+// in server:
+/**
+ * server.js
+ * model filde => db
+ * schema folder =>
+ * npm init --y
+ * npm i express mongoose body-parser nodemon
+ */
