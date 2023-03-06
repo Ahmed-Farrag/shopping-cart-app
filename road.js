7201377214c33e3e1d069a30e080c2d91c87c84d
Ahmed Farrag <47854268+Ahmed-Farrag@users.noreply.github.com>
2023-03-06 13:13:18 +0200

    gulp and sass in client project

--- road.js
+++ road.js
@@ -8,7 +8,23 @@
  * create component
  * pages component 
  * to redux make data, store folder
+ * * Gulp: gulpjs.com : it's a task runner - tool make repited functions 
+ *     make minfication to css, js code and compreazing to imgs and make compiling to sass code 
+ * * Sass: pre prosessor - dunumic css - variables and func
+ 
+ *>npm i gulp gulp-sass sass
+ * creat in src/ file:  .scss
+ * create in app : gulpfile.js
+ * put 3 require and write function
+ * in package.json : 'sass':'gulp sass'
+
+
+
+
+
  */ 
+
+
 
 // in server:
 /**
