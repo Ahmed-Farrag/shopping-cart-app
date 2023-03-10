7f0d03a06734b143acc6214bd6615881971054d1
Ahmed Farrag <47854268+Ahmed-Farrag@users.noreply.github.com>
2023-03-10 20:57:10 +0200

    redux start

--- Server/Server.js
+++ Server/Server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
-// const Product = require("./Models/ProductModule");
 const router = require("./Routes/routes");
 
 const app = express();
