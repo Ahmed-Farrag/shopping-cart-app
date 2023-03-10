49a6ce0531dccf8d8ff62d2028d171786d6e174c
Ahmed Farrag <47854268+Ahmed-Farrag@users.noreply.github.com>
2023-03-10 20:29:54 +0200

    merged issuse

--- Server/Server.js
+++ Server/Server.js
@@ -18,22 +18,6 @@ mongoose
   })
   .then((res) => console.log("connection Done"));
 
-// app.get("/api/products", async (req, res) => {
-//   const products = await Product.find({});
-//   res.send(products);
-// });
-
-// app.post("/api/products", async (req, res) => {
-//   const newProduct = new Product(req.body);
-//   const saveP = await newProduct.save();
-//   res.send(saveP);
-// });
-
-// app.delete("/api/products/:id", async (req, res) => {
-//   const deleteProduct = await Product.findByIdAndDelete(req.params.id);
-//   res.send(deleteProduct);
-// });
-
 app.listen(5000, () => {
   console.log("Running Done");
 });
