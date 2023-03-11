fb0d05a646bd5155f3ac33cb260a9c99a9dd93df
Ahmed Farrag <47854268+Ahmed-Farrag@users.noreply.github.com>
2023-03-11 14:28:09 +0200

    redux start

--- shopping-react/between.js
+++ shopping-react/between.js
@@ -1,5 +1,22 @@
 /*
-HEADLINE: REDUX
+HEADLINE: REDUX : state mangment sys
+اباسب البروبس من البيرنت للاب للاب لاب props driling: 
+the propblem is : اني بباسي بروبس من البيرنت لكل الشيلد بتوعه
+
+* $ the selotion:
+state file have all data and any comp need this data can access it
+
+
+1.create store
+
+
+
+
+
+
+
+
+
 
 
 */
