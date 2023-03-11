cc24b7711dabb53b4744594c41c55c01bc92e4d5
Ahmed Farrag <47854268+Ahmed-Farrag@users.noreply.github.com>
2023-03-11 17:38:10 +0200

    fix bug

---
+++ Server/Schema/ProductSchema.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  title: String,
+  imageUrl: String,
+  desc: String,
+  price: Number,
+  sizes: [String],
+});
+
+module.exports = productSchema;
